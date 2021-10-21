72f1773ae99ddcb3ba98ec0dab5f6ba735160cb6
EricKeenan <eric.keenan@colorado.edu>
2021-10-21 09:45:09 -0600

    Update workflow to accept 1 km grids as input instead of 30 km

--- PIG/run.sh
+++ PIG/run.sh
@@ -11,9 +11,6 @@ uly=26700
 lrx=-1339200
 lry=-137700
 
-# Atmospheric forcing resolution
-tgt_res=30000 # unit meters
-
 # Source DEM path
 src_dem_path=$2
 
@@ -34,10 +31,10 @@ sed -i "/^input_speed_grid/s/$/${ts}_vel.asc/" PIG.cfg
 sed -i "/^input_dir_grid/s/$/${ts}_ang.asc/" PIG.cfg
 
 cp ../PIG.prj ./input/${ts}_vel.prj # Wind speed
-gdal_translate -of AAIGrid -tr ${tgt_res} ${tgt_res} ${meteo_dir}/${ts}.vw ./input/${ts}_vel.asc
+cp ${meteo_dir}/${ts}.vw ./input/${ts}_vel.asc
 
 cp ../PIG.prj ./input/${ts}_ang.prj # Wind direction
-gdal_translate -of AAIGrid -tr ${tgt_res} ${tgt_res} ${meteo_dir}/${ts}.dw ./input/${ts}_ang.asc
+cp ${meteo_dir}/${ts}.dw ./input/${ts}_ang.asc
 
 gdal_translate -of GTiff -a_nodata -9999 -projwin ${ulx} ${uly} ${lrx} ${lry} ${src_dem_path} ./input/PIG.tif # DEM
 
